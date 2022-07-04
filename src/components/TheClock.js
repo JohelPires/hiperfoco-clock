c7c20558d661db13adb82fc91d04d13a75007d82
JohelPires <johelpires@gmail.com>
2022-07-04 14:38:59 -0400

    first commit, main clock component

---
+++ src/components/TheClock.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TheClock() {
+  return (
+    <div className='theclock'>
+      <h2>Session</h2>
+      <h1>23:59</h1>
+    </div>
+  )
+}
+
+export default TheClock
