952a66c358663a46592d857b1368c1c6a9aef209
JohelPires <johelpires@gmail.com>
2022-07-04 19:48:23 -0400

    update the timer to setInterval useEffect

--- src/components/TheClock.js
+++ src/components/TheClock.js
@@ -1,10 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   BsArrowCounterclockwise,
   BsPauseFill,
   BsPlayFill,
 } from 'react-icons/bs'
-import { useTimer } from 'use-timer'
 
 function TheClock({
   sessionLength,
@@ -12,36 +11,32 @@ function TheClock({
   setSessionLength,
   setBreakLength,
 }) {
-  const { time, start, pause, reset, status } = useTimer({
-    initialTime: sessionLength,
-    timerType: 'DECREMENTAL',
-  })
+  const [counter, setCounter] = useState(60)
 
-  function toggleTimer() {
-    if (status === 'PAUSED' || status === 'STOPPED') {
-      start()
-    } else {
-      pause()
-    }
-  }
+  // Third Attempts
+  React.useEffect(() => {
+    const timer =
+      counter > 0 && setInterval(() => setCounter((prevC) => prevC - 1), 1000)
+    return () => clearInterval(timer)
+  }, [counter])
 
   function handleReset() {
     //stop timer
     setSessionLength(25)
     setBreakLength(5)
-    reset()
+  }
+  function toggleTimer() {
+    setCounter(0)
   }
   return (
     <div className='theclock'>
       <h2 className='title' id='timer-label'>
         Session
       </h2>
-      <h1 id='time-left'>
-        {timeLeft} - {time}
-      </h1>
+      <h1 id='time-left'>{counter}</h1>
       <div className='settings-container'>
         <h2 onClick={toggleTimer} className='btn' id='start_stop'>
-          {status === 'RUNNING' ? <BsPauseFill /> : <BsPlayFill />}
+          <BsPauseFill /> <BsPlayFill />
         </h2>
         <h2 onClick={handleReset} className='btn' id='reset'>
           <BsArrowCounterclockwise />
